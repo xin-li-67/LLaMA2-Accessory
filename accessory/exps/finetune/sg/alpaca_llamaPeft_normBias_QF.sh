b3d98b646bb6396b72e6d7f0ca2ed029bab561e5
kriskrisliu <2786203491@qq.com>
2023-08-30 11:37:42 +0800

    update training scripts

--- accessory/exps/finetune/sg/alpaca_llamaPeft_normBias_QF.sh
+++ accessory/exps/finetune/sg/alpaca_llamaPeft_normBias_QF.sh
@@ -15,7 +15,7 @@ mkdir -p output/"$exp_name"
 
 torchrun --master_port=1112 --nproc_per_node=6 main_finetune.py \
 --output_dir output/"$exp_name" --epochs 4 --warmup_epochs 1 \
---batch_size 8 --accum_iter 1 --num_workers 4 \
+--batch_size 4 --accum_iter 2 --num_workers 4 \
 --max_words 512 \
 --lr 0.00005 --min_lr 0.000005 --clip_grad 2 --weight_decay 0.02 \
 --data_parallel "$data_parallel" --model_parallel_size "$model_parallel" --checkpointing \
@@ -23,6 +23,7 @@ torchrun --master_port=1112 --nproc_per_node=6 main_finetune.py \
 --no_visual \
 --pretrained_path "$pretrained_path" --pretrained_type="$pretrained_type" \
 --data_config $data_config \
+--quant --only_save_trainable \
 2>&1 | tee -a output/"$exp_name"/output.log
 
 echo "exp name: $exp_name"
